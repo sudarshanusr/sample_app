57bebf61a2921b9b8f30f4f82b575dfcd80b802f
sudarshan <sudarshan@dispatchtrack.com>
2016-12-18 11:53:28 +0530

    “First Commit”

---
+++ db/migrate/20161218054047_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :bio
+      t.integer :age
+
+      t.timestamps null: false
+    end
+  end
+end
